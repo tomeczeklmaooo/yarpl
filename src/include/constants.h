a69b9b5566404d41556c337fa90dbbcae6c76209
tomeczeklmaooo <tomeczeklmao@proton.me>
2024-10-31 21:52:49 +0100

    tokens no longer enum, add constants

---
+++ src/include/constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const float PI = 3.14159;
+const char digits[11] = "0123456789";
+const char chars_lower[27] = "abcdefghijklmnopqrstuwvxyz";
+const char chars_upper[27] = "ABCDEFGHIJKLMNOPQRSTUWVXYZ";
+const char chars_special[33] = "`~!@#$%^&*()-_=+[]{}\\|:;\'\",<.>/?";
+
+#endif
--- src/include/tokens.h
+++ src/include/tokens.h
@@ -1,26 +1,22 @@
 #ifndef TOKENS_H
 #define TOKENS_H
 
-enum token_type {
-	// single character tokens
-	LPAREN, RPAREN, LBRACE, RBRACE,
-	COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
-
-	// one or two character tokens
-	AND, OR,
-	NOT, NOT_EQUAL,
-	EQUAL, EQUAL_EQUAL,
-	GREATER, GREATER_EQUAL,
-	LESS, LESS_EQUAL,
-
-	// literals
-	IDENTIFIER, STRING, NUMBER,
-
-	// keywords
-	PRINT, DECL, SET, SCAN, INC, FOR, IF, ELIF,
-	ELSE, SWAP, CONV, RET, CONTINUE, FUNC, END, BREAK,
-
-	EOF
+// a table of all tokens (obviously)
+char *tokens[] = {
+	"INT",
+	"FLOAT",
+	"PLUS",
+	"MINUS",
+	"MUL",
+	"DIV",
+	"LPAREN",
+	"RPAREN"
 };
+
+/*
+	@param type: token type, char *tokens
+	@param value: token value, idk, return none if no value
+*/
+char* return_token(char type, char value);
 
 #endif
